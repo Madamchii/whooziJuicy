32b9e6f0649650ecf08c0726dcc2fd3bf0a4405c
Madamchii <valetiad@gmail.com>
2016-11-07 19:56:39 +0200

    Add files via upload

---
+++ whooziJuicy.py
@@ -0,0 +1,64 @@
+
+class Person:    # attributes
+    __letter = ""
+    __age = 0
+    __allowed = true or false
+
+    def __init__(self, letter, age, allowed):
+        self.__letter = letter
+        self.__age = age
+        self.__allowed = allowed
+        
+    def person()
+        age = 0
+        allowed = false
+        
+    def set_letter(self, letter):
+        self.__letter = letter
+
+    def get_letter(self):
+        return self.__letter
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_age(self):
+        return str(self.__age)
+
+    def set_allowed(self, allowed):
+        self.__allowed = allowed
+
+    def is_allowed(self):
+        return self.__allowed
+    
+        
+class WhooziJuicy:
+    def whooziJuicy():
+        numPeople = 0
+    def processEntryRules
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
